60e868e970a4dc66f156450d69f97aa79bae3a72
QuanshengZeng <zengqs@mail.nankai.edu.cn>
2023-04-30 20:35:46 +0800

    update

--- mask2former/data/datasets/register_dm_seg.py
+++ mask2former/data/datasets/register_dm_seg.py
@@ -98,3 +98,10 @@ register_dm_seg(_root, "dm10_combine_sub-4_seg_train", "DiffuseMade10",
 register_dm_seg(_root, "dm10_combine_sub-5_seg_train", "DiffuseMade10",
                 "out_combine/out_ann_dir/tanh2-0.25-4.0-dcrf-0.05-0.95",
                 "imageset/seg_3/good.txt")
+
+register_dm_seg(_root, "dm10_clipes_seg_train", "DiffuseMade10",
+                "clipes_pseudo_masks_aug")
+
+register_dm_seg(_root, "dm10_combine_clip_fix_box-nms_p600", "DiffuseMade10",
+                "out_combine/out_ann_dir/tanh2-0.25-4.0-dcrf-0.05-0.95",
+                "imageset/clip_fix_box-nms_p600/good.txt")
